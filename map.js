ac3f7ddc23fb184f5ee9392b9f7d5d57e2e776e0
Peter <peterstrbik97@gmail.com>
2016-03-30 01:52:20 -0400

    TIMING

--- map.js
+++ map.js
@@ -1,12 +1,25 @@
 var m = 1;
-var d;
+var d = 0;
 var currday = document.getElementById("currday");
 var states = [];
 
 d3.csv("2016DemPrimary.csv", function(rows){
     states = rows;
     console.log(states)
-    doStuff();
+    setInterval( function() {
+	d += 1;
+	if (d == months[m]){
+	    m += 1;
+	    d = 1;
+	}
+	for (var i = 0; i < states.length; i++){
+	    if (states[i].Month == m && states[i].Day == d){
+		console.log(states[i].fips);
+	    }
+	}
+	currday.textcontent = m + "/" + d + "/16";
+    }, 1000);
+
 });
 
 //number of days in months in 2016
@@ -15,22 +28,25 @@ var months = [0, 31, 29, 31, 30, 31, 30] //everything after June is irrelevant
 
 
 var doStuff = function(){
-    setInterval(function() {d += 1;}, 2000); 
-    while (m <= 6){
-	d = 1;
-	while (d <= months[m]){ //goes through each day in each month Jan->June
-		for (var i = 0; i < states.length; i++){
-			if (states[i].Month == m && states[i].Day == d){
-			    console.log(states[i].fips);
-			}
+};
+
+//var doStuff = function(){
+   // setInterval(function() {d += 1;}, 2000); 
+   // while (m <= 6){
+//	d = 1;
+//	while (d <= months[m]){ //goes through each day in each month Jan->June
+//		for (var i = 0; i < states.length; i++){
+//			if (states[i].Month == m && states[i].Day == d){
+//			    console.log(states[i].fips);
+//			}
 		//		#state held the primary that day
 		//		color_state(i[1], i[4], i[5])
 		
 		//wait 1 second
 		//console.log("a day passed.");
-		currday.textcontent = m + "/" + d + "/16";
-		}
-	}
-	m += 1
-    }
-};
+//		currday.textcontent = m + "/" + d + "/16";
+//		}
+//	}
+//	m += 1
+//    }
+//};
