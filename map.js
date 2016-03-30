f8a5ad950a50195bbf7b66df8f78c009ce6da56a
Peter <peterstrbik97@gmail.com>
2016-03-30 01:33:00 -0400

    HALP

--- map.js
+++ map.js
@@ -1,24 +1,36 @@
 var m = 1;
 var d;
-var i;
 var currday = document.getElementById("currday");
+var states = [];
+
+d3.csv("2016DemPrimary.csv", function(rows){
+    states = rows;
+    console.log(states)
+    doStuff();
+});
 
 //number of days in months in 2016
 //0 is placeholder so [1] == january
 var months = [0, 31, 29, 31, 30, 31, 30] //everything after June is irrelevant
 
-while (m <= 6){
+
+var doStuff = function(){
+    setInterval(function() {d += 1;}, 2000); 
+    while (m <= 6){
 	d = 1;
 	while (d <= months[m]){ //goes through each day in each month Jan->June
-		//for i in len(dem_data):
-		//	if i[2] == m and i[3] == d: 
+		for (var i = 0; i < states.length; i++){
+			if (states[i].Month == m && states[i].Day == d){
+			    console.log(states[i].fips);
+			}
 		//		#state held the primary that day
 		//		color_state(i[1], i[4], i[5])
 		
 		//wait 1 second
-		console.log("a day passed.");
+		//console.log("a day passed.");
 		currday.textcontent = m + "/" + d + "/16";
-		d += 1;
+		}
 	}
 	m += 1
-}
+    }
+};
