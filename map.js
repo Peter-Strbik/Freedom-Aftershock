c2cea4f97536c810225f7046f9a0bf0a4af34796
Krzysztof Hochlewicz <khochlewicz@stuy.edu>
2016-03-30 01:10:19 -0400

    JS still broken, but sensical.

--- map.js
+++ map.js
@@ -10,14 +10,15 @@ var months = [0, 31, 29, 31, 30, 31, 30] //everything after June is irrelevant
 while (m <= 6){
 	d = 1;
 	while (d <= months[m]){ //goes through each day in each month Jan->June
-		for (i = 1; i <= months[m]; i++ ){
-			//if i[2] == m and i[3] == d: 
-				//state held the primary that day
-				//color_state(i[1], i[4], i[5])
-			setTimeout(function() {d += 1;}, 1000);
-			console.log("a day passed.");
-			currday.textcontent = m + "/" + d + "/16";
-		}
-	m += 1
+		//for i in len(dem_data):
+		//	if i[2] == m and i[3] == d: 
+		//		#state held the primary that day
+		//		color_state(i[1], i[4], i[5])
+		
+		//wait 1 second
+		console.log("a day passed.");
+		currday.textcontent = m + "/" + d + "/16";
+		d += 1;
 	}
+	m += 1
 }
